9a48a09a33388ba4546f3381c7748cd9bbaf1fb5
Logan Jones <loganemilyjones@mac.com>
2020-11-11 10:48:42 -0800

    fix linting issues

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,14 @@
 // Modal js
-console.log("hello");
+console.log('hello');
 
-const modal = document.getElementById("modal");
-const modalButton = document.getElementById("editButton");
+const modal = document.getElementById('modal');
+const modalButton = document.getElementById('editButton');
 modalButton.onclick = function() {
-    modal.style.display = "block";
-}
+  modal.style.display = 'block';
+};
 
 window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
+};
